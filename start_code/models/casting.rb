7145d67b4722edf469ea2c5d47b84c717370f1ad
Catherine Hall <catherine1992123@hotmail.com>
2019-12-05 15:10:05 +0000

    Set up three models

---
+++ start_code/models/casting.rb
@@ -0,0 +1,13 @@
+require('pg')
+require_relative('../db/sql_runner')
+
+class Casting
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @movie_id = options['movie_id'].to_i
+    @star_id = options['star_id'].to_i
+    @fee = options['fee'].to_i
+  end
+
+end
